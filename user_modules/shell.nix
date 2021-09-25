1158e65ecb21bf096a315ac423872e2bce3dbd30
Brian Shu <littlebubu.shu@gmail.com>
2021-09-25 12:39:10 -0400

    ok

--- user_modules/shell.nix
+++ user_modules/shell.nix
@@ -1,2 +1,11 @@
-{ ... }:
-{}
+{ options, config, lib, pkgs, ... }:
+
+let
+  cfg = config.modules.shell;
+  inherit (lib) mkOption;
+in
+{
+  options.modules.shell.enable = mkOption {
+    type = types.nullOr (types.enum [ "bash" "zsh" "fish" ]);
+  };
+}
