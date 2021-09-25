bc7bebbad7d5b2e986a37a45891b96976bbf7ae0
Brian Shu <littlebubu.shu@gmail.com>
2021-09-25 19:34:05 -0400

    fix

--- modules/options.nix
+++ modules/options.nix
@@ -8,7 +8,7 @@ in
   options = {
     dotfiles = {
       # we use strings so the path is nice and doesn't point to nix store
-      dir = {
+      dir = mkOption {
         type = types.str;
         default = (
           lib.findFirst builtins.pathExists (toString ../.) [
@@ -34,9 +34,9 @@ in
 
   config = {
     environment.variables = {
-      DOTFILES = toString config.dotfiles.dir;
-      DOTFILES_BIN = toString config.dotfiles.binDir;
-      PATH = [ (toString config.dotfiles.binDir) ];
+      DOTFILES = config.dotfiles.dir;
+      DOTFILES_BIN = config.dotfiles.binDir;
+      PATH = [ config.dotfiles.binDir ];
     };
   };
 }
