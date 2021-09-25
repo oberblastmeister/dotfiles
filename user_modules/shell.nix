c54be28c279875ebcfc3627f3ca2424f9a0194b8
Brian Shu <littlebubu.shu@gmail.com>
2021-09-25 12:48:35 -0400

    add warning

--- user_modules/shell.nix
+++ user_modules/shell.nix
@@ -8,8 +8,17 @@ in
   options.modules.shell.enable = mkOption {
     type = types.nullOr (types.enum [ "bash" "zsh" "fish" ]);
   };
-  
+
   config = {
+    warnings =
+      if config.modules.shell.enable == null
+      then [
+        ''
+          You must select a shell for `modules.shell.enable` because home-manager needs to manage the shell
+          for things like `sessionVariables` and `shellAliases` to work.
+        ''
+      ]
+      else [];
     programs.${cfg.enable}.enable = true;
   };
 }
