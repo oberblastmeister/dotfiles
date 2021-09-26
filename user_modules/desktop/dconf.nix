0e52b24004fc10f74287fa7c01f74e52563c9a88
Brian Shu <littlebubu.shu@gmail.com>
2021-09-26 16:24:43 -0400

    autopush

--- user_modules/desktop/dconf.nix
+++ user_modules/desktop/dconf.nix
@@ -2,9 +2,75 @@
 
 let
   cfg' = config'.modules.desktop.gnome;
+  mkTuple = lib.hm.gvariant.mkTuple;
 in
 {
   config = lib.mkIf cfg'.enable {
-    
+    dconf.settings = {
+      "org/gnome/desktop/wm/keybindings" = {
+        close = [ "<Super>q" ];
+        maximize = [ "<Alt><Super>k" ];
+        minimize = [];
+        move-to-monitor-down = [ "<Shift><Super>braceleft" ];
+        move-to-monitor-left = [ "<Shift><Super>braceright" ];
+        move-to-workspace-1 = [ "<Shift><Super>exclam" ];
+        move-to-workspace-2 = [ "<Shift><Super>at" ];
+        move-to-workspace-3 = [ "<Shift><Super>numbersign" ];
+        move-to-workspace-4 = [ "<Shift><Super>dollar" ];
+        move-to-workspace-left = [ "<Alt><Super>bracketleft" ];
+        move-to-workspace-right = [ "<Alt><Super>bracketright" ];
+        show-desktop = [ "<Shift><Super>h" ];
+        switch-to-workspace-1 = [ "<Super>1" ];
+        switch-to-workspace-2 = [ "<Super>2" ];
+        switch-to-workspace-3 = [ "<Super>3" ];
+        switch-to-workspace-4 = [ "<Super>4" ];
+        switch-to-workspace-left = [ "<Super>bracketleft" ];
+        switch-to-workspace-right = [ "<Super>bracketright" ];
+        toggle-fullscreen = [ "<Super>f" ];
+        toggle-maximized = [ "<Super>m" ];
+        unmaximize = [ "<Alt><Super>j" ];
+      };
+      "org/gnome/mutter/keybindings" = {
+        toggle-tiled-left = [ "<Alt><Super>h" ];
+        toggle-tiled-right = [ "<Alt><Super>l" ];
+      };
+      "org/gnome/shell/keybindings" = {
+        # conflicting keybindings
+        toggle-message-tray = [];
+        toggle-overview = [];
+      };
+      "org/gnome/settings-daemon/plugins/media-keys" = {
+        control-center = [ "<Super>s" ];
+        www = [ "<Super>b" ];
+      };
+      "org/gnome/desktop/interface" = {
+        clock-show-seconds = false;
+        clock-show-weekday = true;
+        cursor-theme = "Adwaita";
+        font-antialiasing = "grayscale";
+        font-hinting = "slight";
+        gtk-theme = "Orchis";
+        icon-theme = "ePapirus";
+        show-battery-percentage = true;
+      };
+      "org/gnome/shell/extensions/user-theme" = {
+        name = "Orchis";
+      };
+      "org/gnome/desktop/input-sources" = {
+        xkb-options = [ "ctrl:nocaps" ];
+      };
+      "org/gnome/desktop/peripherals/touchpad" = {
+        disable-while-typing = false;
+        two-finger-scrolling-enabled = true;
+      };
+      "org/gnome/shell" = {
+        disable-user-extensions = false;
+        enabled-extensions = [
+          "user-theme@gnome-shell-extensions.gcampax.github.com"
+          "clipboard-indicator@tudmotu.com"
+          "drive-menu@gnome-shell-extensions.gcampax.github.com"
+        ];
+      };
+    };
   };
 }
