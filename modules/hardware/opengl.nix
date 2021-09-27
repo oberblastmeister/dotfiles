a67a7f2657293f58022cf6efbc4a5ae009b3a5f0
Brian Shu <littlebubu.shu@gmail.com>
2021-09-27 11:01:23 -0400

    .

---
+++ modules/hardware/opengl.nix
@@ -0,0 +1,37 @@
+{ config, options, unstable, pkgs, lib, my, ... }:
+
+let
+  cfg = config.modules.hardware.opengl;
+in
+{
+  options.modules.hardware.opengl = {
+    enable = my.options.mkEnable;
+    enableIntel = my.options.mkEnable;
+  };
+
+  config = lib.mkMerge [
+    (
+      lib.mkIf cfg.enable {
+        hardware.opengl = {
+          enable = true;
+          extraPackages = with pkgs; [
+            libGL
+            vaapiVdpau
+            libvdpau-va-gl
+          ];
+        };
+      }
+    )
+
+    (
+      lib.mkIf cfg.enableIntel {
+        hardware.opengl = {
+          extraPackages = with pkgs; [
+            pkgs.vaapiIntel
+            pkgs.intel-media-driver
+          ];
+        };
+      }
+    )
+  ];
+}
