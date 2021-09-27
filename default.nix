f18d2c94dded2a06e110f6bb380e1c1d8a2a6355
Brian Shu <littlebubu.shu@gmail.com>
2021-09-26 20:08:51 -0400

    .

--- default.nix
+++ default.nix
@@ -28,16 +28,24 @@ in
     };
   };
 
-  programs ={
+  programs = {
     fish.enable = true;
     zsh.enable = true;
   };
   users.defaultUserShell = pkgs.fish;
-  
+
   security = {
     sudo.enable = mkDefault false;
     doas = {
       enable = mkDefault true;
+      extraRules = {
+        groups = [ "wheel" ];
+        # allow environment variables to be kept
+        # must be kept on or stuff like NIX_PATH will fail
+        keepEnv = true;
+        # allow password to be skipped for a while
+        persist = true;
+      };
     };
   };
 
