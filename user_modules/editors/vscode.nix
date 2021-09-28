1ab18792dcee8ccd5ec3335b33ce5483d66adefd
Brian Shu <littlebubu.shu@gmail.com>
2021-09-28 12:52:55 -0400

    .

--- user_modules/editors/vscode.nix
+++ user_modules/editors/vscode.nix
@@ -30,7 +30,8 @@ in
     # home.file."${toDir}/settings.json".source = config.lib.file.mkOutOfStoreSymlink (codeDir + "/settings.json");
     # home.file."${toDir}/keybindings.json".source = config.lib.file.mkOutOfStoreSymlink (codeDir + "/keybindings.json");
 
-    home.file."${toDir}/settings.json".source = codeDir + "/settings.json";
-    home.file."${toDir}/keybindings.json".source = codeDir + "/keybindings.json";
+    # home.file."${toDir}/settings.json".source = codeDir + "/settings.json";
+    # home.file."${toDir}/keybindings.json".source = codeDir + "/keybindings.json";
+    home.file."test.md".source = config'.dotfiles.configDir + "/test.md";
   };
 }
