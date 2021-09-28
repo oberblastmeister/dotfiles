79364cc04c3d32ab25634b75777896f877d0d0b2
Brian Shu <littlebubu.shu@gmail.com>
2021-09-28 12:47:25 -0400

    .

--- user_modules/editors/vscode.nix
+++ user_modules/editors/vscode.nix
@@ -26,8 +26,11 @@ in
       ];
     };
 
-    home.file."${toDir}/settings.json".source = config.lib.file.mkOutOfStoreSymlink (codeDir + "/settings.json");
     # home.file."$[toDir/settings.json".source = config.lib.file.mkOutOfStoreSymlink (../../config/)
-    home.file."${toDir}/keybindings.json".source = config.lib.file.mkOutOfStoreSymlink (codeDir + "/keybindings.json");
+    # home.file."${toDir}/settings.json".source = config.lib.file.mkOutOfStoreSymlink (codeDir + "/settings.json");
+    # home.file."${toDir}/keybindings.json".source = config.lib.file.mkOutOfStoreSymlink (codeDir + "/keybindings.json");
+
+    home.file."${toDir}/settings.json".source = codeDir + "/settings.json";
+    home.file."${toDir}/keybindings.json".source = codeDir + "/keybindings.json";
   };
 }
