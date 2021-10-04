b007240f8fda2fb580918cef046810808f4754a2
Brian Shu <littlebubu.shu@gmail.com>
2021-10-04 18:31:41 -0400

    .

--- user_modules/desktop/apps/flameshot.nix
+++ user_modules/desktop/apps/flameshot.nix
@@ -2,13 +2,29 @@
 
 let
   cfg = config.modules.desktop.apps.flameshot;
+  iniFormat = pkgs.formats.ini { };
+  inherit (lib) mkOption;
+  configDir = config.xdg.configHome;
 in
 {
   options.modules.desktop.apps.flameshot = {
     enable = my.options.mkEnable;
+    settings = mkOption {
+      type = iniFormat.type;
+      default = { };
+    };
   };
 
   config = lib.mkIf cfg.enable {
     services.flameshot.enable = true;
+
+    modules.desktop.apps.flameshot.settings = {
+      showHelp = false;
+      showSidePanelButton = false;
+      showStartupLaunchMessage = false;
+    };
+    home.file."${configDir}/flameshot/flameshot.ini" = mkIf (cfg.settings != { }) {
+      source = iniFormat.generate "flameshot.ini" cfg.settings;
+    };
   };
 }
