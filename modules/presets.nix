2eb675c9163b9ae35885c1409424ad95ad8adeef
Brian Shu <littlebubu.shu@gmail.com>
2021-10-04 17:19:01 -0400

    .

--- modules/presets.nix
+++ modules/presets.nix
@@ -20,9 +20,6 @@ in
 
   config = lib.mkIf (cfg.enable != null) (
     lib.mkMerge [
-      {
-        home.file."test.md".source = abort "adfasdf";
-      }
       (lib.mkIf (cfg.enable == "full") fullPreset)
       (lib.mkIf (cfg.enable == "minimal") minimalPreset)
     ]
