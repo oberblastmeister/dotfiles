977b5cf53cdaefc37b2b913e03b4e2e5461aaae8
Brian Shu <littlebubu.shu@gmail.com>
2021-10-04 17:50:51 -0400

    .

--- user_modules/presets.nix
+++ user_modules/presets.nix
@@ -7,8 +7,8 @@ let
   fullPreset = {
     modules = {
       editors = {
-        vim.enable = mkDefault true;
-        vscode.enable = mkDefault true;
+        vim.enable = mkDefault false;
+        vscode.enable = mkDefault false;
       };
       dev = {
         rust.enable = mkDefault true;
