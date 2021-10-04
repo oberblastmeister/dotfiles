824d45d8d764f0630ad1879577eebe68242f6f3d
Brian Shu <littlebubu.shu@gmail.com>
2021-10-04 16:22:39 -0400

    .

--- user_modules/desktop/apps/flameshot.nix
+++ user_modules/desktop/apps/flameshot.nix
@@ -9,27 +9,30 @@ in
     enable = my.options.mkEnable;
     settings = mkOption {
       type = iniFormat.type;
-      default = {
-        General = {
-          contrastUiColor = "#458588";
-          uiColor = "#83a598";
-          drawColor = "#8ec07c";
-          disabledTrayIcon = false;
-          drawThickness = 2;
-          # saveAfterCopyPath=/home/brian/Pictures
-          # savePath=/home/brian/Pictures
-          # savePathFixed=false;
-          # setSaveAsFileExtension=Portable Network Graphic file (PNG) (*.png)
-          showHelp = false;
-          showSidePanelButton = false;
-          showStartupLaunchMessage = false;
-        };
-      };
+      default = { };
     };
   };
 
   config = lib.mkIf cfg.enable {
     services.flameshot.enable = true;
+
+    modules.desktop.apps.flameshot.settings = {
+      General = {
+        contrastUiColor = "#458588";
+        uiColor = "#83a598";
+        drawColor = "#8ec07c";
+        disabledTrayIcon = false;
+        drawThickness = 2;
+        # saveAfterCopyPath=/home/brian/Pictures
+        # savePath=/home/brian/Pictures
+        # savePathFixed=false;
+        # setSaveAsFileExtension=Portable Network Graphic file (PNG) (*.png)
+        showHelp = false;
+        showSidePanelButton = false;
+        showStartupLaunchMessage = false;
+      };
+
+    };
 
     xdg.configFile."flameshot/flameshot.ini".source = iniFormat.generate "flameshot-settings.ini" cfg.settings;
   };
