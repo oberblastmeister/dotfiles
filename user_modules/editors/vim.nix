51d06a15b15dffdc1f1a0d2031449a9b4bf54c6d
Brian Shu <littlebubu.shu@gmail.com>
2021-10-04 17:35:24 -0400

    .

--- user_modules/editors/vim.nix
+++ user_modules/editors/vim.nix
@@ -1,32 +1,33 @@
-{ config, config', my, lib, pkgs, inputs, ... }:
+# { config, config', my, lib, pkgs, inputs, ... }:
 
-let
-  cfg = config.modules.editors.vim;
-  inherit (lib) mkOption types;
-  inherit (config.lib.file) mkOutOfStoreSymlink;
-in
-{
-  options.modules.editors.vim = {
-    enable = my.options.mkEnable;
-  };
+# let
+#   cfg = config.modules.editors.vim;
+#   inherit (lib) mkOption types;
+#   inherit (config.lib.file) mkOutOfStoreSymlink;
+# in
+# {
+#   options.modules.editors.vim = {
+#     enable = my.options.mkEnable;
+#   };
 
-  config = lib.mkIf cfg.enable {
-    nixpkgs.overlays = [ inputs.neovim-nightly-overlay.overlay ];
+#   config = lib.mkIf cfg.enable {
+#     nixpkgs.overlays = [ inputs.neovim-nightly-overlay.overlay ];
 
-    home.packages = with pkgs; [
-      neovim
-      neovim-remote
-      nodePackages.neovim
-      python39Packages.pynvim
-    ];
+#     home.packages = with pkgs; [
+#       neovim
+#       neovim-remote
+#       nodePackages.neovim
+#       python39Packages.pynvim
+#     ];
 
-    modules.shell.aliases = {
-      "vim" = "nvim";
-      "vi" = "nvim";
-    };
+#     modules.shell.aliases = {
+#       "vim" = "nvim";
+#       "vi" = "nvim";
+#     };
 
-    xdg.configFile = {
-      "nvim".source = mkOutOfStoreSymlink (config'.dotfiles.naiveConfigDir + "/nvim");
-    };
-  };
-}
+#     xdg.configFile = {
+#       "nvim".source = mkOutOfStoreSymlink (config'.dotfiles.naiveConfigDir + "/nvim");
+#     };
+#   };
+# }
+(abort "wat")
