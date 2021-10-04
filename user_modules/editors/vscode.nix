9b6b3b0ccf9e35cc09c07e4704c47aac7cdd8bb1
Brian Shu <littlebubu.shu@gmail.com>
2021-10-04 17:34:53 -0400

    .

--- user_modules/editors/vscode.nix
+++ user_modules/editors/vscode.nix
@@ -1,57 +1,59 @@
-{ config, config', my, lib, pkgs, inputs, ... }:
+# { config, config', my, lib, pkgs, inputs, ... }:
 
-let
-  cfg = config.modules.editors.vscode;
-  naiveCodeDir = config'.dotfiles.naiveConfigDir + "/Code";
-  toDir = "${config.xdg.configHome}/Code/User";
-  inherit (lib) mkOption types;
-  inherit (config.lib.file) mkOutOfStoreSymlink;
-  iniFormat = pkgs.formats.ini { };
-  jsonFormat = pkgs.formats.json { };
-  configDir = config.xdg.configHome;
-in
-{
-  options.modules.editors.vscode = {
-    enable = my.options.mkEnable;
-  };
+# let
+#   cfg = config.modules.editors.vscode;
+#   naiveCodeDir = config'.dotfiles.naiveConfigDir + "/Code";
+#   toDir = "${config.xdg.configHome}/Code/User";
+#   inherit (lib) mkOption types;
+#   inherit (config.lib.file) mkOutOfStoreSymlink;
+#   iniFormat = pkgs.formats.ini { };
+#   jsonFormat = pkgs.formats.json { };
+#   configDir = config.xdg.configHome;
+# in
+# {
+#   options.modules.editors.vscode = {
+#     enable = my.options.mkEnable;
+#   };
 
-  config = lib.mkIf cfg.enable {
-    programs.vscode = {
-      enable = true;
-      extensions = with pkgs.vscode-extensions; [
-        jnoortheen.nix-ide
-        arrterian.nix-env-selector
-        asvetliakov.vscode-neovim
-        bodil.file-browser
-        eamodio.gitlens
-        kahole.magit
-        streetsidesoftware.code-spell-checker
-      ];
-    };
+#   config = lib.mkIf cfg.enable {
+#     programs.vscode = {
+#       enable = true;
+#       extensions = with pkgs.vscode-extensions; [
+#         jnoortheen.nix-ide
+#         arrterian.nix-env-selector
+#         asvetliakov.vscode-neovim
+#         bodil.file-browser
+#         eamodio.gitlens
+#         kahole.magit
+#         streetsidesoftware.code-spell-checker
+#       ];
+#     };
 
-    home.file."${toDir}/settings.json".source = mkOutOfStoreSymlink (naiveCodeDir + "/settings.json");
-    home.file."${toDir}/keybindings.json".source = mkOutOfStoreSymlink (naiveCodeDir + "/keybindings.json");
-    home.file."${toDir}/broh.json".source = mkOutOfStoreSymlink (naiveCodeDir + "/keybindings.json");
-    home.file."test.md".source = ./test.md;
-  };
+#     home.file."${toDir}/settings.json".source = mkOutOfStoreSymlink (naiveCodeDir + "/settings.json");
+#     home.file."${toDir}/keybindings.json".source = mkOutOfStoreSymlink (naiveCodeDir + "/keybindings.json");
+#     home.file."${toDir}/broh.json".source = mkOutOfStoreSymlink (naiveCodeDir + "/keybindings.json");
+#     home.file."test.md".source = ./test.md;
+#   };
 
 
-  # home.file."test.ini" = /* mkIf (cfg.settings != { }) */ {
-  #   source = jsonFormat.generate "flameshot.ini" {
-  #     General = {
-  #       contrastUiColor = "#458588";
-  #       uiColor = "#83a598";
-  #       drawColor = "#8ec07c";
-  #       disabledTrayIcon = false;
-  #       drawThickness = 2;
-  #       # saveAfterCopyPath=/home/brian/Pictures
-  #       # savePath=/home/brian/Pictures
-  #       # savePathFixed=false;
-  #       # setSaveAsFileExtension=Portable Network Graphic file (PNG) (*.png)
-  #       showHelp = false;
-  #       showSidePanelButton = false;
-  #       showStartupLaunchMessage = false;
-  #     };
-  #   };
-  # };
-}
+#   # home.file."test.ini" = /* mkIf (cfg.settings != { }) */ {
+#   #   source = jsonFormat.generate "flameshot.ini" {
+#   #     General = {
+#   #       contrastUiColor = "#458588";
+#   #       uiColor = "#83a598";
+#   #       drawColor = "#8ec07c";
+#   #       disabledTrayIcon = false;
+#   #       drawThickness = 2;
+#   #       # saveAfterCopyPath=/home/brian/Pictures
+#   #       # savePath=/home/brian/Pictures
+#   #       # savePathFixed=false;
+#   #       # setSaveAsFileExtension=Portable Network Graphic file (PNG) (*.png)
+#   #       showHelp = false;
+#   #       showSidePanelButton = false;
+#   #       showStartupLaunchMessage = false;
+#   #     };
+#   #   };
+#   # };
+# }
+
+abort "wat"
