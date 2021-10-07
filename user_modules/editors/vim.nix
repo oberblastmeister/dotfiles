985e879a5063ede2dd571be04f05dd5ee913d0af
Brian Shu <littlebubu.shu@gmail.com>
2021-10-07 18:04:20 -0400

    .

--- user_modules/editors/vim.nix
+++ user_modules/editors/vim.nix
@@ -17,7 +17,18 @@ in
       enable = true;
       plugins = with pkgs.vimPlugins; [
         vim-surround
-        vim-commentary
+        {
+          plugin = vim-commentary;
+          config = ''
+            xmap <Leader>c  <Plug>Commentary
+            nmap <Leader>c  <Plug>Commentary
+            omap <Leader>c  <Plug>Commentary
+            nmap <Leader>cc <Plug>CommentaryLine
+            nmap <Leader>cu <Plug>Commentary<Plug>Commentary
+
+            noremap gc <Nop>
+          '';
+        }
         targets-vim
       ];
       viAlias = true;
