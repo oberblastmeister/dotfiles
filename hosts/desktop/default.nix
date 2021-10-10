45d2fee8549e574085ba687d158c6ce10702916c
brian <littlebubu.shu@gmail.com>
2021-10-10 21:57:45 +0000

    .

---
+++ hosts/desktop/default.nix
@@ -0,0 +1,15 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  users.users.brian = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+  };
+
+  modules.presets.enable = "full";
+  home-manager.users.brian.modules.presets.enable = "full";
+}
