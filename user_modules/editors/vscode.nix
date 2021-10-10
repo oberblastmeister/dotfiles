d1be86e143edc9218926996efb878548151f049d
Brian Shu <littlebubu.shu@gmail.com>
2021-10-10 17:24:30 -0400

    .

--- user_modules/editors/vscode.nix
+++ user_modules/editors/vscode.nix
@@ -15,7 +15,7 @@ in
 
   config = lib.mkIf cfg.enable {
     programs.vscode = {
-      enable = false;
+      enable = true;
       extensions = with pkgs.vscode-extensions; [
         jnoortheen.nix-ide
         arrterian.nix-env-selector
