d61d0518d1560aa48d0c7f93de1ea78db2a85689
brian <littlebubu.shu@gmail.com>
2021-10-10 23:55:11 +0000

    .

--- user_modules/editors/default.nix
+++ user_modules/editors/default.nix
@@ -1,26 +1,25 @@
-{}
-# { options, config, unstable, pkgs, lib, my, ... }:
+{ options, config, unstable, pkgs, lib, my, ... }:
 
-# let
-#   cfg = config.modules.editors;
-#   inherit (lib) mkOption types;
-# in
-# {
-#   options.modules.editors = {
-#     default = mkOption {
-#       type = types.str;
-#       default = "vim";
-#     };
-#     visual = mkOption {
-#       type = types.str;
-#       default = "code";
-#     };
-#   };
+let
+  cfg = config.modules.editors;
+  inherit (lib) mkOption types;
+in
+{
+  options.modules.editors = {
+    default = mkOption {
+      type = types.str;
+      default = "vim";
+    };
+    visual = mkOption {
+      type = types.str;
+      default = "code";
+    };
+  };
 
-#   config = {
-#     home.sessionVariables = {
-#       EDITOR = cfg.default;
-#       VISUAL = cfg.visual;
-#     };
-#   };
-# }
+  config = {
+    home.sessionVariables = {
+      EDITOR = cfg.default;
+      VISUAL = cfg.visual;
+    };
+  };
+}
