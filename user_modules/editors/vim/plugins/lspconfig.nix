e07c60cf9decd71a87df1a59f66dd119a12a68b6
Brian Shu <littlebubu.shu@gmail.com>
2021-10-11 16:14:12 -0400

    .

--- user_modules/editors/vim/plugins/lspconfig.nix
+++ user_modules/editors/vim/plugins/lspconfig.nix
@@ -1,5 +1,13 @@
-{ vimPlugins }:
+{ config, vimPlugins }:
 
+let
+  writeIf = cond: s: if cond then s else "";
+  cfg = config.modules.dev;
+in
 {
   plugin = vimPlugins.nvim-lspconfig;
+  config = ''
+    ${writeIf cfg.python.enable "lspconfig.pyright.setup()"}
+  '';
 }
+
