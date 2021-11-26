debc17afe17df5c904532681c5622db480fcf0d0
brian <littlebubu.shu@gmail.com>
2021-11-26 18:59:29 -0500

    update nixpkgs-unstable and update flameshot with it

--- user_modules/desktop/apps/flameshot.nix
+++ user_modules/desktop/apps/flameshot.nix
@@ -8,30 +8,26 @@ in
 {
   options.modules.desktop.apps.flameshot = {
     enable = my.options.mkEnable;
-    settings = mkOption {
-      type = iniFormat.type;
-      default = { };
-    };
   };
 
   config = lib.mkIf cfg.enable {
-    services.flameshot.enable = true;
-
-    modules.desktop.apps.flameshot.settings = {
-      General = {
-        uiColor = "#83a598";
-        drawColor = "#8ec07c";
-        contrastUiColor = "#458588";
-        showHelp = false;
-        showSidePanelButton = false;
-        showStartupLaunchMessage = false;
-        disabledTrayIcon = false;
-        drawThickness = 2;
+    services.flameshot = {
+      enable = true;
+      # use unstable to avoid cannot capture screen issue
+      # https://github.com/flameshot-org/flameshot/issues/1910
+      package = unstable.flameshot;
+      settings = {
+        General = {
+          uiColor = "#83a598";
+          drawColor = "#8ec07c";
+          contrastUiColor = "#458588";
+          showHelp = false;
+          showSidePanelButton = false;
+          showStartupLaunchMessage = false;
+          disabledTrayIcon = false;
+          drawThickness = 2;
+        };
       };
-    };
-
-    home.file."${config.xdg.configHome}/flameshot/flameshot.ini" = lib.mkIf (cfg.settings != { }) {
-      source = iniFormat.generate "flameshot.ini" cfg.settings;
     };
   };
 }
