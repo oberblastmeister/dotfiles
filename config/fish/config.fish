0cd7f7b67405d54d0d9e5f3eca648f744305d091
brian <littlebubu.shu@gmail.com>
2022-06-12 08:21:58 -0400

    .

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,2 +1,22 @@
-fish_vi_key_bindings
 set fish_greeting # disable greeting
+
+function ranger-cd
+    set dir (mktemp -t ranger_cd.XXX)
+    ranger --choosedir=$dir
+    cd (cat $dir) $argv
+    rm $dir
+    commandline -f repaint
+end
+
+function fish_user_key_bindings
+    # Execute this once per mode that emacs bindings should be used in
+    fish_default_key_bindings -M insert
+
+    # Then execute the vi-bindings so they take precedence when there's a conflict.
+    # Without --no-erase fish_vi_key_bindings will default to
+    # resetting all bindings.
+    # The argument specifies the initial mode (insert, "default" or visual).
+    fish_vi_key_bindings --no-erase insert
+
+    bind -M insert \co ranger-cd
+end
