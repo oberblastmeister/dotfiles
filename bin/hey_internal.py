c67e542d97d72ec7c8025ae9ee193af5f15ea267
brian <littlebubu.shu@gmail.com>
2022-07-09 13:36:15 -0400

    use click for hey

--- bin/hey_internal.py
+++ bin/hey_internal.py
@@ -3,22 +3,83 @@ import pathlib
 from pathlib import Path
 import subprocess
 from argparse import ArgumentParser
-from typing import Any, Tuple
+from typing import Any, Optional, Tuple, TypeVar, cast
 import shutil
 import os
 import sys
 from dataclasses import dataclass
 import click
 
+T = TypeVar("T")
+
+
+def unwrap(optional: Optional[T]) -> T:
+    if optional is None:
+        raise TypeError("Optional value is None")
+    return optional
+
 
 def is_root() -> bool:
     return os.getuid() == 0
 
 
-def check_is_root() -> None:
-    if not is_root():
-        print("You must be root to run this subcommand!")
-        sys.exit(1)
+@click.group(invoke_without_command=True)
+@click.version_option()
+@click.option(
+    "--flake",
+    type=click.Path(exists=True),
+    default=pathlib.Path(__file__).parent.parent.resolve(),
+)
+def cli(flake):
+    """
+    Helper for nix
+    """
+    if is_root():
+        raise click.ClickException(
+            "You must not be root. hey will ask you for your password when root is needed."
+        )
+
+
+@cli.command("gc")
+def cli_gc():
+    subprocess.run(["sudo", "nix-collect-garbage", "-d"])
+    subprocess.run(["sudo", "nix-store", "--optimise"])
+
+
+def run_rebuild(flake: Path, extra: list[str]):
+    cmd = ["sudo", "nixos-rebuild", "--flake", flake] + extra
+    subprocess.run(cmd)
+
+
+def run_update(flake: Path):
+    subprocess.run(["nix", "flake", "update", flake])
+
+
+@cli.command("update")
+@click.pass_context
+def cli_update(cx: click.Context):
+    run_update(unwrap(cx.parent).params["flake"])
+
+
+@cli.command("upgrade")
+@click.pass_context
+def cli_upgrade(cx: click.Context):
+    flake = unwrap(cx.parent).params["flake"]
+    run_update(flake)
+    run_rebuild(flake, ["switch"])
+
+
+@cli.command("rollback")
+@click.pass_context
+def cli_rollback(cx: click.Context):
+    run_rebuild(unwrap(cx.parent).params["flake"], ["--rollback", "switch"])
+
+
+@cli.command("rebuild")
+@click.argument("--extra", nargs=-1)
+@click.pass_context
+def cli_rebuild(cx: click.Context, extra):
+    run_rebuild(unwrap(cx.parent).params["flake"], list(extra))
 
 
 def generate_hardware_config(root: Path, dir: Path) -> None:
@@ -29,264 +90,70 @@ def generate_hardware_config(root: Path, dir: Path) -> None:
     (dir / "configuration.nix").unlink()
 
 
-@dataclass(frozen=True)
-class Gc:
-    pass
-
-    def run(self) -> None:
-        check_is_root()
-        subprocess.run(["nix-collect-garbage", "-d"])
-        subprocess.run(["nix-store", "--optimise"])
+@cli.command("push")
+def cli_push():
+    subprocess.run(["git", "add", "."])
+    subprocess.run(["git", "commit", "-m", "."])
+    subprocess.run(["git", "push"])
 
 
-@dataclass(frozen=True)
-class Rebuild:
-    extra: list[str]
-
-    def run(self, flake: Path) -> None:
-        check_is_root()
-        subprocess.run(["nixos-rebuild", "--flake", str(flake)] + self.extra)
-
-
-@dataclass(frozen=True)
-class Push:
-    pass
-
-    def run(self) -> None:
-        subprocess.run(["git", "add", "."])
-        subprocess.run(["git", "commit", "-m", "."])
-        subprocess.run(["git", "push"])
-
-
-@dataclass(frozen=True)
-class Update:
-    pass
-
-    def run(self, flake: Path) -> None:
-        subprocess.run(["nix", "flake", "update", flake])
-
-
-@dataclass(frozen=True)
-class Upgrade:
-    pass
-
-    def run(self, flake: Path) -> None:
-        check_is_root()
-        Update().run(flake)
-        Rebuild(["switch"]).run(flake)
-
-
-@dataclass(frozen=True)
-class RollBack:
-    pass
-
-    def run(self, flake: Path) -> None:
-        Rebuild(["--rollback", "switch"]).run(flake)
-
-
-@dataclass(frozen=True)
-class AddHost:
-    hostname: str
-    hardware_conf: None | str
-    conf: None | str
-    root: None | Path
-    force: bool
-
-    def run(self, flake: Path) -> None:
-        check_is_root()
-        if len(Path(self.hostname).parts) != 1:
-            raise ValueError("The name must only have one path component")
-        host_path = flake / "hosts" / self.hostname
-        if host_path.exists():
-            if self.force:
-                shutil.rmtree(host_path)
-            else:
-                raise ValueError("The host path already exists")
-        host_path.mkdir()
+@click.command("add-host")
+@click.argument("hostname")
+@click.option("--force", is_flag=True)
+@click.option("--conf", type=click.Path(exists=True))
+@click.option("--hardware-conf", type=click.Path(exists=True))
+@click.option("--root", type=click.Path(exists=True))
+def cli_add_host(
+    cx: click.Context,
+    hostname,
+    force,
+    conf,
+    hardware_conf,
+    root,
+):
+    flake = unwrap(cx.parent).params["flake"]
+    if len(Path(hostname).parts) != 1:
+        raise ValueError("The name must only have one path component")
+    host_path = flake / "hosts" / hostname
+    if host_path.exists():
+        if force:
+            shutil.rmtree(host_path)
+        else:
+            raise ValueError("The host path already exists")
+    host_path.mkdir()
+    try:
+        if hardware_conf is None:
+            generate_hardware_config(root or Path("/"), host_path)
+        else:
+            shutil.copy(hardware_conf, host_path / "hardware-configuration.nix")
+        if conf is None:
+            shutil.copy(flake / "host_template" / "default.nix", host_path)
+        else:
+            shutil.copy(conf, host_path / "default.nix")
+    except Exception:
         try:
-            if self.hardware_conf is None:
-                generate_hardware_config(self.root or Path("/"), host_path)
-            else:
-                shutil.copy(
-                    self.hardware_conf, host_path / "hardware-configuration.nix"
-                )
-            if self.conf is None:
-                shutil.copy(flake / "host_template" / "default.nix", host_path)
-            else:
-                shutil.copy(self.conf, host_path / "default.nix")
+            host_path.rmdir()
         except Exception:
-            try:
-                host_path.rmdir()
-            except Exception:
-                pass
-            raise
-        subprocess.run(["git", "add", host_path])
+            pass
+        raise
+    subprocess.run(["git", "add", host_path])
 
 
-@dataclass(frozen=True)
-class Cd:
-    pass
-
-    def run(self) -> None:
-        subprocess.run(["cd", os.environ["DOTFILES"]])
-
-
-@dataclass(frozen=True)
-class UpdateDconf:
-    force: bool
-
-    def run(self) -> None:
-        dconf_dir = Path(os.environ["DOTFILES_CONFIG"]) / "dconf"
-        dconf_settings = dconf_dir / "dconf.settings"
-        if dconf_settings.exists() and not self.force:
-            raise Exception("dconf.settings exists, pass --force to override it")
-        with dconf_settings.open(mode="w") as file:
-            subprocess.run(["dconf", "dump", "/"], stdout=file)
-        dconf_nix = dconf_dir / "dconf.nix"
-        if dconf_nix.exists() and not self.force:
-            raise Exception("dconf.nix exists, pass --force to override it")
-        subprocess.run(["dconf2nix", "-i", dconf_settings, "-o", dconf_nix])
+@cli.command("update-dconf")
+@click.option("--force", is_flag=True)
+@click.pass_context
+def cli_update_dconf(cx: click.Context, force):
+    dconf_dir = Path(os.environ["DOTFILES_CONFIG"]) / "dconf"
+    dconf_settings = dconf_dir / "dconf.settings"
+    if dconf_settings.exists() and not force:
+        raise click.ClickException("dconf.settings exists, pass --force to override it")
+    with dconf_settings.open(mode="w") as file:
+        subprocess.run(["dconf", "dump", "/"], stdout=file)
+    dconf_nix = dconf_dir / "dconf.nix"
+    if dconf_nix.exists() and not force:
+        raise click.ClickException("dconf.nix exists, pass --force to override it")
+    subprocess.run(["dconf2nix", "-i", dconf_settings, "-o", dconf_nix])
 
 
-@dataclass(frozen=True)
-class Help:
-    parser: ArgumentParser
-
-    def run(self) -> None:
-        self.parser.print_help()
-
-
-Subcmd = (
-    Gc
-    | Rebuild
-    | Push
-    | Update
-    | Upgrade
-    | RollBack
-    | AddHost
-    | Help
-    | Cd
-    | UpdateDconf
-)
-
-
-@dataclass(frozen=True)
-class Opt:
-    flake: Path
-    subcmd: Subcmd
-
-    def run(self) -> None:
-        match self.subcmd:
-            case Gc():
-                self.subcmd.run()
-            case Update():
-                self.subcmd.run(self.flake)
-            case Upgrade():
-                self.subcmd.run(self.flake)
-            case RollBack():
-                self.subcmd.run(self.flake)
-            case AddHost():
-                self.subcmd.run(self.flake)
-            case Push():
-                self.subcmd.run()
-            case Rebuild():
-                self.subcmd.run(self.flake)
-            case Cd():
-                self.subcmd.run()
-            case UpdateDconf():
-                self.subcmd.run()
-            case Help():
-                self.subcmd.run()
-
-
-def get_parser() -> ArgumentParser:
-    parser = argparse.ArgumentParser(description="Helper for nix")
-    defaultFlake = pathlib.Path(__file__).parent.parent.resolve()
-    parser.add_argument(
-        "--flake",
-        type=Path,
-        nargs="?",
-        help="Optionaly specify where the flake is",
-        default=defaultFlake,
-    )
-    # parser.add_argument('--host', type=str, nargs='?',
-    #                     help='Optionally specify the host', default=None)
-    subparsers = parser.add_subparsers(dest="subcmd")
-    _ = subparsers.add_parser("gc")
-    _ = subparsers.add_parser("update")
-    _ = subparsers.add_parser("upgrade")
-    _ = subparsers.add_parser("rebuild")
-    _ = subparsers.add_parser("cd")
-    update_dconf_parser = subparsers.add_parser("update-dconf")
-    update_dconf_parser.add_argument(
-        "--force", action="store_true", help="Override the file"
-    )
-    add_host_parser = subparsers.add_parser("add-host")
-    add_host_parser.add_argument("hostname", type=str, help="Required hostname")
-    add_host_parser.add_argument(
-        "--hardware-conf", type=Path, help="hardware-configuration.nix path"
-    )
-    add_host_parser.add_argument(
-        "--conf",
-        type=Path,
-        help="configuration.nix path. Will be copied to hosts/<host>/default.nix",
-    )
-    add_host_parser.add_argument(
-        "--root", type=Path, help="the root to use", default="/"
-    )
-    add_host_parser.add_argument(
-        "--force", action="store_true", help="Override an existing host"
-    )
-    _ = subparsers.add_parser("push")
-    return parser
-
-
-def is_flake_dir(path: Path) -> None:
-    assert path.is_dir(), "Flake path is not a directory"
-    assert path.joinpath(
-        "flake.nix"
-    ).exists(), "flake.nix does not exist in the directory"
-
-
-def from_args() -> Opt:
-    parser = get_parser()
-    args = vars(parser.parse_args())
-    args["flake"] = args["flake"].resolve(strict=True)
-    is_flake_dir(args["flake"])
-    subcmd = None
-    match args["subcmd"]:
-        case "gc":
-            subcmd = Gc()
-        case "update":
-            subcmd = Update()
-        case "upgrade":
-            subcmd = Upgrade()
-        case "rollback":
-            subcmd = RollBack()
-        case "add-host":
-            subcmd = AddHost(
-                args["hostname"],
-                args.get("hardware_conf"),
-                args.get("conf"),
-                args.get("root"),
-                args["force"],
-            )
-        case "push":
-            subcmd = Push()
-        case "rebuild":
-            subcmd = Rebuild(["switch"])
-        case "cd":
-            subcmd = Cd()
-        case "update-dconf":
-            subcmd = UpdateDconf(args["force"])
-        case _:
-            subcmd = Help(parser)
-    return Opt(args["flake"], subcmd)
-
-
-def main() -> None:
-    from_args().run()
-
-
-if __name__ == "__main__":
-    main()
+def main():
+    cli()
--- bin/shell.nix
+++ bin/shell.nix
@@ -1,8 +1,11 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import <nixpkgs-unstable> { } }:
 
 pkgs.mkShell {
   # nativeBuildInputs is usually what you want -- tools you need to run
   nativeBuildInputs = with pkgs; [
-    (ghc.withPackages (pkgs: with pkgs; [ turtle optparse-applicative unix ]))
+    (haskellPackages.ghcWithPackages (pkgs: with pkgs; [ turtle optparse-applicative unix ]))
+    python310
+    # we need unstable for this to fix the list bug
+    python310Packages.typer
   ];
 }
