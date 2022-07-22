857b8bd2591977a3ffedd4532937743c2524ae8b
brian <littlebubu.shu@gmail.com>
2022-07-22 10:38:56 -0400

    kmonad configuration

--- modules/kmonad.nix
+++ modules/kmonad.nix
@@ -2,9 +2,8 @@
 let
   cfg = config.modules.kmonad;
   inherit (lib.my.options) mkEnable;
-  keyboard = { name, ... }: {
-
-  };
+  keyboard = { name, ... }: { };
+  kmonadDir = config.dotfiles.configDir + "/kmonad";
 in
 {
   imports = [ inputs.kmonad.nixosModules.default ];
@@ -12,10 +11,18 @@ in
   options.modules.kmonad = {
     enable = mkEnable;
   };
-  
+
   config = lib.mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
       kmonad
     ];
+
+    services.kmonad = {
+      enable = true;
+      keyboards.keychron = {
+        device = "/dev/input/by-id/usb-Keychron_Keychron_Q1-if02-event-kbd";
+        config = builtins.readFile (kmonadDir + "/keychron.kbd");
+      };
+    };
   };
 }
